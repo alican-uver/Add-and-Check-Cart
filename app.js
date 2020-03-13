cd6147720ff6c02c0d05ec3b3338ccd3ac926b49
Alican Uver <can.uverr@gmail.com>
2020-03-13 23:09:48 +0300

    Güncelleme yapıldı

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     ui.setUpApp();
 
-   request.getData()
-   .then((products) => {
+    request.getData()
+    .then((products) => {
         ui.showProductsOnloadPage(products);
         Storage.productsAddStorage(products);
 
-   })
-   .then(() => { // Second then use is important beacuse this functions needs to check after page loads   
-       ui.getBagBtn();
-       ui.cartLogic();
-   })
-   
-   .catch((err) => {
-       console.log(err);      
-   });
+    })
+    .then(() => { // Second then use is important beacuse this functions needs to check after page loads   
+        ui.getBagBtn();
+        ui.cartLogic();
+    })
+    
+    .catch((err) => {
+        console.log(err);      
+    });
 
 });
